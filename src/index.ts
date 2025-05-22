e42639108251d670894b7276b48af39ee363f458
unknown <sagarsonar2508@gmail.com>
2025-05-22 22:45:36 +0530

    host

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ server.get('/health', async (request, reply) => {
 
 const start = async () => {
   try {
-    await server.listen({ port: 3000 });
+    await server.listen({ port: 3000, host: '0.0.0.0' });
     console.log('🚀 Server running at http://localhost:3000');
   } catch (err) {
     server.log.error(err);
