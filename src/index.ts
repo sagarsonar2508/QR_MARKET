a9b0b77d8e0509f71be0c12f7cda7e128dff9462
unknown <sagarsonar2508@gmail.com>
2025-05-25 23:17:09 +0530

    tp

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 import Fastify from 'fastify';
+import { Router } from './routes/route.modules';
 
 const server = Fastify({ logger: true });
-
-// Health check route
-server.get('/health', async (request, reply) => {
-  return { status: 'ok', timestamp: Date.now() };
-});
+const router = new Router(server);
+router.registerRoutes();
 
 const start = async () => {
-  try {
-    await server.listen({ port: 3000, host: '0.0.0.0' });
-    console.log('🚀 Server running at http://localhost:3000');
-  } catch (err) {
-    server.log.error(err);
-    process.exit(1);
-  }
+    try {
+        await server.listen({ port: 3000, host: '0.0.0.0' });
+        console.log('🚀 Server running at http://localhost:3000');
+    } catch (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
 };
 
 start();
---
+++ src/routes/health.route.ts
@@ -0,0 +1,46 @@
+import { FastifyInstance } from "fastify";
+
+export class Router {
+    private server: FastifyInstance;
+
+    constructor(server: FastifyInstance) {
+        this.server = server;
+    }
+
+    public registerRoutes() {
+        this.server.get('/health', async (request, reply) => {
+            return { status: 'ok', timestamp: Date.now() };
+        });
+        this.server.get('/n', async (request, reply) => {
+            reply.type('text/html').send(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Nikhil Chutiya</title>
+                <style>
+                body {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    margin: 0;
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                }
+                h1 {
+                    font-size: 3rem;
+                    font-weight: bold;
+                    color: #333;
+                }
+                </style>
+            </head>
+            <body>
+                <h1>Nikhil</h1>
+            </body>
+            </html>
+            `);
+        });
+    }
+}
